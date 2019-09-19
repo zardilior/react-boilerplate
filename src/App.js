f1b9b5dd9515271fc2d3dc32d13bcebda229c756
joseMac <josefort@2tor.com.mx>
2019-09-19 10:33:54 -0500

    added initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
-import DatosPersonales from "./DatosPersonales/";
-import TablaCalificaciones from "./TablaCalificaciones/";
-import Datos from "./Datos/";
 
 class App extends Component{
-  constructor(){
-      super();
-      this.state = {
-        datos:Datos
-      }
-  }
   render(){
     return(
       <div className="App">
-        <DatosPersonales 
-            alumno={this.state.datos.alumno}>
-        </DatosPersonales>
-        <TablaCalificaciones 
-            calificaciones={this.state.datos.calificaciones}>
-        </TablaCalificaciones>
+        <h1>Boilerplate ready</h1>
       </div>
     );
   }
